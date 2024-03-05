4d3edfeb188ea099f9c69ee871996893a77b6922
WarithV <36975158+WarithV@users.noreply.github.com>
2024-03-05 21:15:40 +0700

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "eb1c1a25ae4ce147468c56f7245d7181",
-"/": "eb1c1a25ae4ce147468c56f7245d7181",
-"main.dart.js": "3c54ea9c7086eb16ff623e996137f937",
+"index.html": "3cf481cf008868ce3015a02fc7d200a0",
+"/": "3cf481cf008868ce3015a02fc7d200a0",
+"main.dart.js": "7995920254e98c3d05633fbf19bae394",
 "manifest.json": "7cfa39b672bb0871ca61106c7c8fe393",
 "version.json": "e9eae465229d9369b7b8b51383aba3d6"
 };
